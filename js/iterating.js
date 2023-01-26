4548e4dfa98d6f849487fa5595c618cb4f2ce08c
Javier Razo <javier.razo1221@gmail.com>
2023-01-26 11:41:00 -0600

    added js

---
+++ js/iterating.js
@@ -0,0 +1,19 @@
+
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    var names = ['Joe', 'Sarah', 'Bob', 'James'];
+    // TODO: Create a log statement that will log the number of elements in the names array.
+    console.log("There are " + names.length + "names in the names array.");
+
+    console.log("For loop:");
+
+    for(var i =0; i < names.length; i++) {
+        console.log("Name at index; " + i + " is " + names[i]);
+     } 
+
+    console.log("ForEach loop:");
+
+    names.forEach(function (element,index, array) {
+        console.log("Name at index: " + index + " is " + element);
+    });
+    
